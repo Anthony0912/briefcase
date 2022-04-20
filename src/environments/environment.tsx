216343bbea0bd872d36de6b3e1437b6828f94e27
Anthony Keylor Cardona Mairena <akcardona0912@outlook.com>
2022-04-19 20:39:32 -0600

    Config deploy in heroku

--- src/environments/environment.tsx
+++ src/environments/environment.tsx
@@ -1,14 +1,14 @@
 //APP_ENVIROMENT
 // development, test, production
 export const environment = {
-    APP_ENVIROMENT:"development",
+    APP_ENVIROMENT:"production",
 
     //## PRODUCTION
-    PROD_URL_API:"",
+    PROD_URL_API:"https://brefcase-api.herokuapp.com/api",
     
 
     //## TEST
-    TEST_URL_API:"",
+    TEST_URL_API:"http://localhost:8000/api",
     
     
     //## DEVELOPMENT
