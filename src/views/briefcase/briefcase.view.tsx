a4098c804341c3760c2f538974304223eeebb30d
Anthony Keylor Cardona Mairena <akcardona0912@outlook.com>
2022-04-23 22:10:41 -0600

    add briefcase calculator

--- src/views/briefcase/briefcase.view.tsx
+++ src/views/briefcase/briefcase.view.tsx
@@ -1,5 +1,6 @@
 import arkanoid from '../../assets/img/arkanoid.png'
 import ticoRides from '../../assets/img/ticoRide.png'
+import Calculadora from '../../assets/img/calculadora.png'
 
 const BriefcaseView = (): JSX.Element => {
   return (
@@ -69,6 +70,42 @@ const BriefcaseView = (): JSX.Element => {
                   >
                     <i className="fab fa-java"></i>
                     <h3 className="title-data-hover">Ir a Arcanoid</h3>
+                  </a>
+                </h5>
+              </div>
+            </div>
+          </figcaption>
+        </figure>
+        <figure className="c4-izmir c4-border-top-right work__img figure">
+          <img src={Calculadora} alt="calculadora" />
+          <figcaption className="c4-layout-bottom-left c4-reveal-right">
+            <div className="content-code">
+              <div className="content-first-code">
+                <h5>
+                  <a
+                    href="https://github.com/Anthony0912/calculator"
+                    className="portafolio__link title-data-hover"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <i className="bx bxl-gitlab"></i> <span className="name-icon">GitLab</span>
+                    <h3 className="title-data-hover">Ver Código Fuente</h3>
+                  </a>
+                </h5>
+              </div>
+              <div>
+                <h5>
+                  <a
+                    href="https://calculation-minimalis.herokuapp.com"
+                    className="portafolio__link title-data-hover"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                     <i className="bx bxl-html5"></i>
+                    <i className="fab fa-php"></i>
+                    <i className="bx bxl-css3"></i>
+                    <i className="bx bxl-javascript"></i>
+                    <h3 className="title-data-hover">Ir a la calculadora</h3>
                   </a>
                 </h5>
               </div>
